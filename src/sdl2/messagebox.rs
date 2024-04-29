dd53be173ae442d397cdf862aa0f7c528555ec8e
Lzu Tao <taolzu@gmail.com>
2024-04-30 00:31:30 +0700

    bump bitflags to 2.5

--- src/sdl2/messagebox.rs
+++ src/sdl2/messagebox.rs
@@ -21,6 +21,7 @@ bitflags! {
 }
 
 bitflags! {
+    #[derive(Debug)]
     pub struct MessageBoxButtonFlag: u32 {
         const ESCAPEKEY_DEFAULT =
             sys::SDL_MessageBoxButtonFlags::SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT as u32;
